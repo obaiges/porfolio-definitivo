1f2fe85ffd90b506545162ac606c1eb4dc27de30
obaiges <oscarbaigesr@gmail.com>
2024-10-26 14:25:26 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './theme-colors.service';
+import { MenuComponent } from "./menu/menu.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Portfolio_v3';
+  title = 'Portfolio v1';
+  colores = [
+    {
+      name: 'Color de fondo',
+      color: '--bg-color'
+    },
+    {
+      name: 'Color secciones',
+      color: '--section-bg-color'
+    },
+    {
+      name: 'Color botones',
+      color: '--button-color'
+    },
+    {
+      name: 'Color de texto',
+      color: '--text-color'
+    },
+    {
+      name: 'Color secundario',
+      color: '--text-secondary-color'
+    },
+    {
+      name: 'Color acentuado',
+      color: '--accent-color'
+    },
+  ]
+
+  constructor(
+    private themeService: ThemeService
+  ) { }
+
+  cambioTema() {
+    this.themeService.toggleTheme()
+  }
 }
---
+++ src/app/theme-colors.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private darkTheme = false;
+
+  toggleTheme(): void {
+    this.darkTheme = !this.darkTheme;
+    if (this.darkTheme) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
+}
